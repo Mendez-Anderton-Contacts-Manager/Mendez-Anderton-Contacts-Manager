be33d311e3e875f26cb2234cbde4046a3af6d242
robertjmendez <98974225+robertjmendez@users.noreply.github.com>
2023-06-30 11:55:53 -0500

    Added functionality to write to contact list

--- src/Contact.java
+++ src/Contact.java
@@ -22,4 +22,8 @@ public class Contact {
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
+
+    public String toString() {
+        return name + " | " + phoneNumber;
+    }
 }
